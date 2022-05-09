3154acd53f7be3e70f48f8a09a8fe1b9b9159617
illusionTBA <illusionztba@gmail.com>
2022-05-09 19:07:12 -0400

    asd

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    botClientID: "972931489777975326",
-    botPrefix: "a!",
-    botowners: ["747588469974499338"/*illusions*/, "948737535931219989", "855611520981008415", "330632835335716877"], // includes illusions, Green, zeltux and green
-    embedColor: "#FFA500",
-    embedfooterText: "Astral by Astral Services",
-    mongoDB: "mongodb://u3sbeu8d3owwsg0pxost:FJBCzPJVhPOmBWFaNuzd@bkyoai2e2phgk8y-mongodb.services.clever-cloud.com:27017/bkyoai2e2phgk8y?retryWrites=true&w=majority"
-}
