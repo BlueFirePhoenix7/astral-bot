d9483e23ff7d152fa5a5cd0a33c637702eadfc4b
pyro <90619153+pyroTM@users.noreply.github.com>
2022-05-12 23:09:52 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botClientID: "971215625034813500",
+    botPrefix: "a!",
+    botowners: ["747588469974499338" /*illusions*/, "948737535931219989", "855611520981008415", "330632835335716877"], // includes illusions, Green, zeltux and green
+    embedColor: "#FFA500",
+    embedfooterText: "Astral by Astral Services",
+    mongoDB: ""
+}
